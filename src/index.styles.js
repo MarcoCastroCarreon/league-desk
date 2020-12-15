8d1433e37aaeb50df727a374f8521009a6795e0a
Marco Castro <mcastrodevelopment@gmail.com>
2020-12-15 11:44:05 -0600

    adding api

--- src/index.styles.js
+++ src/index.styles.js
@@ -9,11 +9,6 @@ export const Header = styles.header`
   font-family: Arial;
 `;
 
-export const Logo = styles.img`
-  src: url('./img/white-thunder.png');
-  float: right;
-`;
-
 export const NavButton = styles.span`
   float: left;
   font-size: 16px;
@@ -25,15 +20,6 @@ export const NavButton = styles.span`
     color: ${(props) => props.focusColor};
     cursor: pointer;
   }
-`;
-
-export const DropdownContentContainer = styles.div`
-  display: none;
-  position: absolute;
-  background-color: #f9f9f9;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
 `;
 
 export const DialogButton = styles.button`
@@ -54,21 +40,11 @@ export const DialogButton = styles.button`
   }
 `;
 
-export const DropdownContentItem = styles.span`
-  float: none;
-  color: black;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-  text-align: left;
-
-  &:hover {
-    background-color: #ddd;
-    cursor: pointer;
-  }
-`;
-
 export const Dialog = styles.div`
   float: left;
   overflow: hidden;
 `;
+
+export const Background = styles.div`
+  border: solid 2px #dc291e;
+`
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,16 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { Grid } from "@material-ui/core";
+
+//Own dependencies
 import { useQuery } from "@apollo/client";
 import { GET_ALL_CHAMPIONS } from "../api/queries";
-import { useEffect } from "react";
+import { Background } from '../index.styles';
 
 const Home = () => {
   const { error, loading, data } = useQuery(GET_ALL_CHAMPIONS);
   const [champs, setChamps] = React.useState([]);
 
-  useEffect(() => {
+  React.useEffect(() => {
     if (!loading) {
       const champions = [...Object.values(data.getChamps)];
       setChamps(champions);
@@ -19,16 +21,28 @@ const Home = () => {
   if (error) return <p>Error: {error}</p>;
 
   return (
-    <Fragment>
-      <Grid container justify="center">
+    <Background>
+      <Grid container justify="center" align="center" style={{ marginTop: 40 }}>
         {champs.length &&
           champs.map((champ) => (
             <Grid item md={4} key={champ.id}>
-              {champ.id}
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <img
+                  src={`http://ddragon.leagueoflegends.com/cdn/10.25.1/img/champion/${champ.id}.png`}
+                  alt={champ.name}
+                />
+              </div>
+              {champ.name}
             </Grid>
           ))}
       </Grid>
-    </Fragment>
+    </Background>
   );
 };
 
